71984b546fb35335d684a33d44c0c62cc325137e
Ge0rges <ge0rges@ge0rges.com>
2018-12-03 21:20:20 -0500

    Pod Update

--- Pods/Target Support Files/Flurry-iOS-SDK/Flurry-iOS-SDK-umbrella.h
+++ Pods/Target Support Files/Flurry-iOS-SDK/Flurry-iOS-SDK-umbrella.h
@@ -1,4 +1,14 @@
+#ifdef __OBJC__
 #import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
 
 #import "FlurryAdBanner.h"
 #import "FlurryAdBannerDelegate.h"
@@ -13,6 +23,7 @@
 #import "FlurryAdDelegate.h"
 #import "Flurry.h"
 #import "FlurrySessionBuilder.h"
+#import "FlurryConsent.h"
 
 FOUNDATION_EXPORT double Flurry_iOS_SDKVersionNumber;
 FOUNDATION_EXPORT const unsigned char Flurry_iOS_SDKVersionString[];
